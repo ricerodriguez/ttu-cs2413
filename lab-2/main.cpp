9a7699fa7af48fc22090db1dec84114e26c7ee67
Victoria Rodriguez <31574111+ricerodriguez@users.noreply.github.com>
2018-06-10 10:10:33 -0500

    Changes to interface
    Interface changes to comply with assignment requirements, program is complete

--- lab-2/main.cpp
+++ lab-2/main.cpp
@@ -1,112 +1,142 @@
 #include "list.h"
 #include <iostream>
+Node::Node(int dat){
+     datum = dat;
+     next = 0;
+}
 
-int main() {
-     List listerine;
-     int choice, size;
-     int dat, ind;
-     // Bizarre issue with delEnd() causes it to only work after the first time it's called,
-     // workaround uses boolean to figure out if it's the first time the user has called the
-     // function and executes twice if it is, then sets the bool to false for the rest of the
-     // program. Hey, whatever works man lol
-     bool first = true;
-     menu:
-     std::cout << "Menu: " << std::endl
-               << "1. View linked list" << std::endl
-               << "2. Insert datum at beginning of list" << std::endl
-               << "3. Insert datum at end of list" << std::endl
-               << "4. Insert datum at specified place in list" << std::endl
-               << "5. Remove datum at beginning of list" << std::endl
-               << "6. Remove datum at end of list" << std::endl
-               << "7. Remove datum at specified place in list" << std::endl
-               << "Type which option to choose." << std::endl;
-     std::cin >> choice;
-     switch(choice) {
-          case 1:
-               std::cout << "Displaying linked list..." << std::endl;
-               try{
-                    listerine.view();
-               }
-               catch(int error) {
-                    std::cerr << "ERROR " << error << ": Linked list is currently empty." << std::endl;
-               }
-               std::cout << "Returning to menu..." << std::endl;
-               goto menu;
+List::List(){
+     this->head = 0;
+     this->size = 0;
+}
 
-          case 2:
-               std::cout << "Inserting datum at the beginning of the list..." << std::endl
-                         << "Enter the datum to insert:" << std::endl;
-               std::cin >> dat;
-               listerine.insBeg(dat);
-               std::cout << "Done. First node of linked list now contains " << listerine.getDat(0) << "." << std::endl
-                         << "Returning to menu..." << std::endl;
-               goto menu;
-
-          case 3:
-               std::cout << "Inserting datum at the end of the list..." << std::endl
-                         << "Enter the datum to insert:" << std::endl;
-               std::cin >> dat;
-               listerine.insEnd(dat);
-               size = listerine.getSize();
-               std::cout << "Done. Last node of linked list now contains " << listerine.getDat(size-1) << "." << std::endl
-                         << "Returning to menu..." << std::endl;
-               goto menu;
-
-          case 4:
-               std::cout << "Inserting datum at specified place in list..." << std::endl
-                         << "Enter the datum to insert:" << std::endl;
-               std::cin >> dat;
-               std::cout << std::endl << "Enter location (index) of linked list to enter " << dat
-                         << " to:" << std::endl << "There are " << listerine.getSize() << " spaces to choose from." << std::endl;
-               std::cin >> ind;
-               try {
-                    listerine.ins(ind,dat);
-               } catch (int error) {
-                    std::cerr << "ERROR " << error << ": Hey listen here numbnuts you can't do that" << std::endl;
-                    goto menu;
-               }
-               std::cout << "Done. Space at " << ind << " now contains " << listerine.getDat(ind) << "." << std::endl
-                         << "Returning to menu..." << std::endl;
-               goto menu;
-
-          case 5:
-               std::cout << "Removing datum from beginning of list..." << std::endl;
-               listerine.delBeg();
-               std::cout << "Done, datum from first node has been removed." << std::endl
-                         << "Returning to menu..." << std::endl;
-               goto menu;
-
-          case 6:
-               std::cout << "Removing datum from end of list..." << std::endl;
-               listerine.delEnd();
-               if(first){
-                   listerine.delEnd(); // Execute twice first time it's called
-                   first=false;
-               }
-               std::cout << "Done, datum from last node has been removed." << std::endl
-                         << "Returning to menu..." << std::endl;
-               goto menu;
-
-          case 7:
-               std::cout << "Removing datum from specified place in list..." << std::endl
-                         << "Enter the location (index) to remove datum from: " << std::endl;
-               std::cin >> ind;
-               try {
-                    listerine.delThis(ind);
-               }
-               catch(int error) {
-                    std::cerr << "ERROR " << error << ": Hey listen here numbnuts you can't do that" << std::endl;
-                    goto menu;
-               }
-               std::cout << "Done. Datum from " << ind << " now empty." << std::endl
-                         << "Returning to menu..." << std::endl;
-               goto menu;
-
-          default:
-               std::cout << "There are 7 options. Choose a number between 1 and 7." << std::endl
-                         << "This shouldn't be this difficult." << std::endl;
-               goto menu;
-
+void List::del(Node *node) {
+     if(node) {
+          this->del(node->next);
+          delete node;
      }
+}
 
+List::~List(){
+     this->del(this->head);
+}
+
+// This works
+void List::ins(int ind, int dat) {
+     if (ind > this->size || ind < 0) {
+          throw 420;
+     }
+     Node *node_0 = new Node(dat);
+     Node *temp = this->head;   // New temporary node, start it by pointing at the head
+     // If index is 0, insert at the beginning
+     if(ind == 0) {
+          node_0->next = this->head; // New node's pointer points to head
+          this->head = node_0;       // Head is updated to point to new node
+     } else {
+          int i;
+          while(i < ind - 1 && temp->next != 0){
+               temp = temp->next;
+               i++;
+          }
+          node_0->next = temp->next;
+          // Make the temp node point to the new node, new node is now being pointed to in the list
+          // and pointing to the next node in the list
+          temp->next = node_0;
+     }
+     this->size++;
+}
+// This works
+void List::insBeg(int dat) {
+     Node *node_0 = new Node(dat);
+     node_0->next = this->head;
+     this->head = node_0;
+     this->size++;
+}
+
+// This works
+void List::insEnd(int dat) {
+     Node *node_0 = new Node(dat);
+     node_0->next = 0;
+     Node *temp = this->head;   // New temporary node, start it by pointing to head
+     if(this->head == 0) {
+          this->head = node_0;    // Head points to new node now
+     } else {
+          while(temp->next != 0){
+               temp = temp->next;
+          }
+          // Make the temp node point to the new node, new node is now being pointed to in the list
+          // and pointing to the next node in the list
+          temp->next = node_0;
+     }
+     this->size++;
+}
+// This works
+void List::delThis(int ind) {
+     Node *kill = this->head;
+     if (ind >= this->size || ind < 0) {
+          throw 420;
+     }
+     if(ind == 0) {
+          this->head = this->head->next;
+     } else {
+          for(int i = 0; kill != 0 && i < ind - 1; i++) {
+               kill = kill->next;
+          }
+          Node *next = kill->next->next;
+          delete kill->next;
+          kill->next = next;
+     }
+     this->size--;
+}
+// This works
+void List::delBeg() {
+     Node *kill;
+     kill = this->head;
+     this->head = this->head->next;
+     this->size--;
+     delete kill;
+}
+// This works, sort of. For some reason doesn't work first time it's used, works every time afterwards...?????
+void List::delEnd() {
+     Node *kill = this->head;
+     if(kill == 0) {
+          throw 2;
+     } else if (kill->next == 0) {
+         delete this->head;
+         this->head = 0;
+     } else {
+          for(int i = 0; kill != 0 && i < size - 1; i++) {
+               kill = kill->next;
+          }
+          // delete kill->next;
+          Node *next = kill->next->next;
+          delete kill->next;
+          kill->next = 0;
+     }
+     this->size--;
+}
+// This works
+void List::view() {
+     Node *temp;
+     int i = 0;
+     if(size) { // Check if there's even anything there
+          for (temp = this->head; temp != 0 && (temp != 0 || i <= size); temp = temp->next)  {
+               i++;
+               std::cout << "Node " << i - 1 << ": " << temp->datum << std::endl;
+          }
+     } else {
+          std::cout << "Linked list is currently empty." << std::endl; // List is empty, nothing to view
+     }
+}
+// This works
+int List::getSize() {
+     return this->size;
+}
+// This works
+int List::getDat(int ind) {
+     Node *temp = head;
+     for(int i = 0; i < ind; i++) {
+          temp = temp->next;
+     }
+     return temp->datum;
 }
